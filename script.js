75296d404e23cac2ac9a2eaca756a98d39a7bb98
jduran7 <josefdb@gmail.com>
2017-12-08 00:53:05 -0500

    add materialize.css, checkmark functionality

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 // var moment = require('moment');
 
 var people = [
-	"Person 1",
-	"Person 2",
-	"Person 3",
-	"Person 4",
-  "Person 5"
+  "Ben",
+  "Chris",
+  "Marco",
+  "Fadi",
+  "Lauren",
+  "Lexi"
 ];
 
-var tasks = ["Task 1", "Task 2", "Task 3", "Task 4"];
+var tasks = [
+
+"GC Agar / GC liquid media prep",
+"Pipette tips and general refills",
+"Trash/recycling",
+"Dish washing",
+"General cleaning/lab order",
+"Update -to order- board (ALL)",
+];
 
 //Returns Monday and Sunday of a week
 //format: dddd is day of the week (text), MMMM is month (text), DD is day of the month (number).
 function getWeek(x,interval) {
-	var firstDay = x.day(-6).format('MMMM DD');
-	var lastDay = x.clone().add(6*interval+(interval-1),"day");
-	if(lastDay.format('MMMM')!= x.format('MMMM')){
-		lastDay = lastDay.format('MMMM DD');
-	}
-	else{
-		lastDay = lastDay.format('DD');
-	}
-	return firstDay + " - " + lastDay;
+  var firstDay = x.day(-6).format('MMMM DD');
+  var lastDay = x.clone().add(6*interval+(interval-1),"day");
+  if(lastDay.format('MMMM')!= x.format('MMMM')){
+    lastDay = lastDay.format('MMMM DD');
+  }
+  else{
+    lastDay = lastDay.format('DD');
+  }
+  return firstDay + " - " + lastDay;
 }
 
 //Sets a list of Mondays and Sundays for x number of upcoming weeks as the keys of the chores object
 function generateList(weeks,interval){
-	var weekList = [];
-	for(i=0;i<weeks/interval;i++){
-		weekList.push(getWeek(moment().clone().add(i*interval,'week'),interval));
-	}
-	return weekList;
+  var weekList = [];
+  for(i=0;i<weeks/interval;i++){
+    weekList.push(getWeek(moment().clone().add(i*interval,'week'),interval));
+  }
+  return weekList;
 }
 
 //implementing the Fisher-Yates shuffle to randomize arrays
@@ -71,7 +80,7 @@ function assignChores(people, tasks, weeks, interval){
   }
 
   for(i=0;i<sortedSequence.length;i++){
-  	finalSchedule[myList[i]] = sortedSequence[i];
+    finalSchedule[myList[i]] = sortedSequence[i];
   }
 
   return finalSchedule;
@@ -84,20 +93,29 @@ var table = '';
 var rows = Object.keys(source);
 var cols = tasks.length;
 
-table += '<tr><td></td>' 
+table += '<thead><tr><td></td>' 
 for(i=0;i<tasks.length;i++){
-  table += '<td>' + tasks[i] + '</td>';
+  table += '<td><b>' + tasks[i] + '</b></td>';
 }
-table+= '</tr>'
+table+= '</tr></thead>'
 
 for(r=0;r<rows.length;r++){
   table += '<tr><td>' + rows[r] + '</td>';
   for(c=0;c<cols;c++){
-    table += '<td>' + source[rows[r]][c] + '</td>'
+    table += '<td><div id="names">' + source[rows[r]][c] + '</div></td>'
   }
   table += '</tr>';
 }
-document.write('<table border=1>' + table + '</table>');
+document.write(' <div id="main"><table class="striped centered">' + table + '</table></div>');
 
-
-// console.log(assignChores(people, tasks, 12, 1));
+$(function(){
+  $('#main').css('cursor','default');
+  $("#main #names").click( function() {
+    $(this).toggleClass("greenLetters"); 
+  if ($(this).hasClass("greenLetters")) {
+    $(this).append("<div class=\"checkmark\">&#10004</div>");
+        } else {
+            $(".checkmark").remove();
+        }
+    });
+});
